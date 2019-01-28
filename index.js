a2c843c726e1f95be3cf584139567b5107d632b1
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-28 03:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  return  `${string.toLowerCase()}`
+  var low = string
+  low.toLowerCase ===low
+  return  `${string}`
 }
